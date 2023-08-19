5d3fef12fd5b310cf3a560a263916de0a0c5c12e
Kalla Mohana Mounika <77119113+KallaMounika18@users.noreply.github.com>
2023-08-19 14:38:58 +0530

    script.js

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
 const apiKey = "8243f9e26a8f50788c219b6009290554";
-const apiUrl1 =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall/timemachine?";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
 const searchBox = document.querySelector(".search-bar input");
 const searchBtn = document.querySelector(".search-bar button");
 const weatherIcon = document.querySelector(".weather-icon");
-const locationBtn = document.getElementById(".get-location");
+const locationBtn = document.querySelector(".get_location");
 
-async function checkWeather(city) {
-  const response = await fetch(apiUrl1 + city + `&appid=${apiKey}`);
+async function getDataWithCity(city) {
+  const response = await fetch(apiUrl + `q=${city}` + `&appid=${apiKey}`);
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   } else {
     var data = await response.json();
-
-    console.log(data);
-
-    show(data);
-    document.querySelector(".weather").style.display = "block";
-  }
-}
-
-async function getData(lat, long) {
-  const promise = await fetch(
-    apiUrl2 + `lat={lat}` + `&lon={lon}` + `&appid={apiKey}`
-  );
-
-  if (response.status == 404) {
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".weather").style.display = "none";
-  } else {
-    var data = await promise.json();
-
     console.log(data);
     show(data);
     document.querySelector(".weather").style.display = "block";
@@ -44,7 +23,7 @@ function show(data) {
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
 
   if (data.weather[0].main == "Clouds") {
     weatherIcon.src = "clouds.png";
@@ -62,11 +41,27 @@ function show(data) {
 }
 
 searchBtn.addEventListener("click", () => {
-  checkWeather(searchBox.value);
+  getDataWithCity(searchBox.value);
 });
 
+async function getDatawithLocation(lat, long) {
+  const promise = await fetch(
+    apiUrl + `lat=${lat}` + `&lon=${long}` + `&appid=${apiKey}`
+  );
+
+  if (promise.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await promise.json();
+    console.log(data);
+    show(data);
+    document.querySelector(".weather").style.display = "block";
+  }
+}
+
 async function gotLocation(position) {
-  const result = await getData(
+  const result = await getDatawithLocation(
     position.coords.latitude,
     position.coords.longitude
   );
@@ -77,12 +72,8 @@ function failedToGet() {
   console.log("There was some issue");
 }
 
-// locationBtn.addEventListener("click", () => {
-//   navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
-// });
-window.onload = function () {
-  locationBtn.addEventListener("click", () => {
+locationBtn.addEventListener("click", () => {
+  if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
-    //navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
-  });
-};
+  else alert("Your brower does not support geolocation api");
+});
